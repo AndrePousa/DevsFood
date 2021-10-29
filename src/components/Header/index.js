452133db7cb206cd655c83d212b69cbae83521ac
AndréPousa <andre.roberto.pousa@gmail.com>
2021-10-28 23:19:22 -0300

    campo de input ok

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { Header, Logo, SearchInput } from './styled';
 
-export default () => {
+export default ({ search, onSearch }) => {
+  
+  const [inputActive, setInputActive] = useState(search == '' ? false : true );
+
+  const handleInputFocus= () => {
+    setInputActive(true);
+  }
+
+  const handleInputBlur = () => {
+    if(search == ''){
+      setInputActive(false)
+    }
+  }
+
+  const handleChange = (e) => {
+    onSearch( e.target.value); 
+  }
+
   return(
     <Header>
       <Logo src="/assets/logo.png"/>
-      <SearchInput type="text" placeholder="Digite aqui..."/>
+      <SearchInput
+        type="text" 
+        placeholder="Digite aqui..."
+        value={search}
+        onChange={handleChange} 
+        active={inputActive}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+      />
     </Header>
   )
-
 }
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -6,6 +6,7 @@ export const Header = styled.div`
   padding:20px;
   display:flex;
   justify-content:space-between;
+  align-items:center;
 `;
 
 export const Logo = styled.img`
@@ -14,6 +15,24 @@ export const Logo = styled.img`
 
 `;
 
+//Campo de procurar
 export const SearchInput = styled.input`
+  border:0;
+  border-radius: 25px;
+  width:${props => props.active ? 300 : 0  }px;
+  height:50px;
+  background-color:#fff;
+  outline:0;  //não indica o campo 
+  background-image:url('/assets/search.png');
+  background-size:30px;
+  background-repeat: no-repeat; //não repete a imagem
+  background-position:10px center;
+  padding-left:50px; //afastou o texto do icone
+  transition:all ease 0.2s;
+  cursor:pointer; //mãozinha
+  font-size:15px;
 
+  &:focus {
+    cursor: text; //curso de texto
+  }
 `;
